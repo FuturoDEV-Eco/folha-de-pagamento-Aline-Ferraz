65f66515965c5a88c17bd3976bc9a6c5dfdcbb8b
Aline Ferraz <153871505+Aline-Ferraz@users.noreply.github.com>
2024-05-25 19:36:08 -0300

    [M2S01] - Ex 5 - Programa principal : Finalizado

--- folha_pagamento/src/index.js
+++ folha_pagamento/src/index.js
@@ -1,3 +1,49 @@
+const calcularINSS = require("./calculo_inss");
+const calcularImpostoRenda = require("./calculo_imposto_renda");
 const calcularSalarioLiquido = require("./calculo_salario_liquido");
 
-console.log(calcularSalarioLiquido(3000, 60.40).toFixed(2));
+const readline = require('readline')
+
+const input = readline.createInterface(
+    process.stdin, 
+    process.stdout
+);
+
+let nomeFuncionario = "";
+let cpfFuncionario = 99999999999;
+let mesPagamento = 0;
+let salarioFuncionario = 0;
+
+input.question("Qual o nome do funcionário? ", (nomeDigitado) => {
+
+    nomeFuncionario = nomeDigitado;
+
+    input.question("Qual é o CPF do funcionário? ", (cpfDigitado) => {
+
+        if (cpfDigitado.length === 11) {
+            cpfFuncionario = cpfDigitado.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+        } else {
+            console.log("CPF digitado é inválido");
+        }
+
+        input.question("Qual é o mês do pagamento? ", (mesDigitado) => {
+            
+            mesPagamento = Number(mesDigitado);
+
+            input.question("Qual é o salário do funcionário? ", (salarioDigitado) => {
+
+                salarioFuncionario = (Number(salarioDigitado));
+
+                console.log("--- Folha de Pagamento ---");
+                console.log(`Nome: ${nomeFuncionario}`);
+                console.log(`CPF: ${cpfFuncionario}`);
+                console.log("Salário Bruto: R$ " + salarioFuncionario.toFixed(2));
+                console.log("INSS: R$ " + calcularINSS(salarioDigitado).toFixed(2));
+                console.log("Imposto de Renda: R$ " + calcularImpostoRenda(salarioDigitado).toFixed(2));
+                console.log("Salário Líquido: R$ " + calcularSalarioLiquido(salarioDigitado).toFixed(2));
+                input.close();
+
+            })
+        })
+    })
+})
